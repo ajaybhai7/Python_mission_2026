a2736d166f244a0770f6829003925b614ec625ad
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 12:17:33 +0530

    Auto-commit: 2026-06-26 12:17:33

--- Cha-7_Loops/0_practice.py
+++ Cha-7_Loops/0_practice.py
@@ -3,7 +3,17 @@
 #  ***
 # *****
 
-n = input("Enter Number: ")
-for i in range(1, n+1):
-    print(" "* n-i, end="")
-    print("*"* 2*i-1)
+# n = int(input("Enter Number: "))
+# for i in range (1, n+1):
+#     print(" "* (n-i), end="")
+#     print("*"* (2*i-1))
+
+
+# Using While Loop
+
+n = int(input("Enter Number: "))
+i = 1
+while i <= n:
+    print(" "* (n-i), end="")
+    print("*"* (2*i-1))
+    i = i+1
