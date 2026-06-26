ea41ab69b89ca4297c1bb0e32d6ecffe44bf98ac
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 20:19:31 +0530

    Auto-commit: 2026-06-26 20:19:31

--- Ch-8_Functions_&_recursions/05_problem05.py
+++ Ch-8_Functions_&_recursions/05_problem05.py
@@ -1,8 +1,9 @@
 # Write a function which converts inches to centimeters(cms)
 
-def inc_cem(inch):
+def inc_cem(n):
     if n == 0:
         return
-    return inch * 2.54
+    return n * 2.54
 n = int(input("Enter Enches to converts in CMS : "))
 print(f"The corresponding value in cms is {inc_cem(n)}")
+
