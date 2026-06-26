117c40fa442e845390a11a19e76959c0a012277e
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 22:23:31 +0530

    Auto-commit: 2026-06-26 22:23:31

--- Ch-8_Functions_&_recursions/07_problem_07.py
+++ Ch-8_Functions_&_recursions/07_problem_07.py
@@ -1,8 +1,17 @@
 # write a python function to print multiplication table of given number.
-def multi(n):
-    num = int(input("Enter Number > "))
-    if num == 0:
+
+# def multiply(n):
+#     for i in range (1,11):
+#         print(f"{n} x {i} = {n*i}")
+# n = int(input("Enter Number: "))
+# multiply(n)
+
+# 
+def multy(n, i=1):
+    if i > 10:
         return
-    i = i+1
-    return num*i
-print(multi())
+    print(f"{n} x {i} = {n*i}")
+    multy(n, i+1)
+n = int(input("Enter Number > "))
+multy(n)
+
