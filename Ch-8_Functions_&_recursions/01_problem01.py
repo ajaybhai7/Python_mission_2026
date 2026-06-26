e62f93c5848d686ab5e9c7c60256ef73f68712b3
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 19:12:02 +0530

    Auto-commit: 2026-06-26 19:12:02

--- Ch-8_Functions_&_recursions/01_problem01.py
+++ Ch-8_Functions_&_recursions/01_problem01.py
@@ -13,3 +13,20 @@ def greatest():
         print("C is greatest Number")
 
 greatest()
+
+
+# In another way 
+
+def greater(a, b, c):
+    if (a > b and a > c):
+        return a
+    elif (b > a and b > c):
+        return b
+    else:
+        return c
+
+a = int(input("Enter Number A : ")) 
+b = int(input("Enter Number B : ")) 
+c = int(input("Enter Number C : ")) 
+
+print(greater(a, b, c))
