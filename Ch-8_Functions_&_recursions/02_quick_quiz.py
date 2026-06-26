640eb59368dc72f9905969fa72a6b7a45a1f6b22
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 18:04:52 +0530

    Auto-commit: 2026-06-26 18:04:52

--- Ch-8_Functions_&_recursions/02_quick_quiz.py
+++ Ch-8_Functions_&_recursions/02_quick_quiz.py
@@ -1,16 +1,24 @@
 # Write a program to greet a user with "Good Day" using function.
 
-def greet():
-    Name = input("Enter you'r name: ")
-    print(f"Good Day {Name}")
+# def greet():
+#     Name = input("Enter you'r name: ")
+#     print(f"Good Day {Name}")
 
-greet()
+# greet()
 
 # in another way
 
-def greet(name):
-    print("Good Day " + name)
+# def greet(name):
+#     print("Good Day " + name)
 
-greet("Ajay")
-greet("Rohan")
-greet("Divya")
+# greet("Ajay")
+# greet("Rohan")
+# greet("Divya")
+
+
+# Again another way with the another argument
+
+def gooday(name, ending):
+    print(f"Good Day {name}")
+    print(ending)
+gooday("Ajay", "Thank You")
