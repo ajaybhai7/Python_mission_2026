2fbe5f04e07006dfc6c97632df10883f0b1f28b2
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 17:45:22 +0530

    Auto-commit: 2026-06-26 17:45:22

---
+++ Ch-8_Functions_&_recursions/01_Function.py
@@ -0,0 +1,9 @@
+def avg():
+    a = int(input("Enter Number : "))
+    b = int(input("Enter Number : "))
+    c = int(input("Enter Number : "))
+
+    average = (a+b+c)/2
+    print(average)
+
+avg()
