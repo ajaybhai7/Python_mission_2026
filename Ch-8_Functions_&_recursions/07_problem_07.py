58fd5124b05425475c7e0b6c3b9922c908eee27a
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 21:35:30 +0530

    Auto-commit: 2026-06-26 21:35:30

---
+++ Ch-8_Functions_&_recursions/07_problem_07.py
@@ -0,0 +1,8 @@
+# write a python function to print multiplication table of given number.
+def multi(n):
+    num = int(input("Enter Number > "))
+    if num == 0:
+        return
+    i = i+1
+    return num*i
+print(multi())
