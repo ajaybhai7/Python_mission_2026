0cea4e710fd36ec58e1d419fdd7062be7cc20d9c
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 20:28:57 +0530

    Auto-commit: 2026-06-26 20:28:57

---
+++ Ch-8_Functions_&_recursions/06_problem06.py
@@ -0,0 +1,8 @@
+# Write a function to remove a given word in a list and strip it at the same time
+
+def rem(l, word):
+    for item in l :
+        return l.remove(word)
+n = input("Enter word to remove : ")
+l = [1, 4, 2, 5, "Ajay", "Arun"]
+print(l, n)
