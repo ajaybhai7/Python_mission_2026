fb7a990119b692f4ec4f8e4159b5911848c6745b
ajaybhai7 <squadterbo@gmail.com>
2026-06-26 12:24:26 +0530

    Auto-commit: 2026-06-26 12:24:26

--- Cha-7_Loops/08_problem08.py
+++ Cha-7_Loops/08_problem08.py
@@ -8,3 +8,13 @@
 n = int(input("Enter Number: "))
 for i in range (1, n+1):
     print(i*"*")
+print("End of the Program")
+
+# Using While Loop
+
+n = int(input("Enter Number > "))
+i = 1
+while i <= n:
+    print(i*"*")
+    i = i+1
+print("End of the Program")
