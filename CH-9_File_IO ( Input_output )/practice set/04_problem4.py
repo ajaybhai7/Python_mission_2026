0f22b8266a7c0e1278316d5ec30e2bb7d8bf82e3
ajaybhai7 <squadterbo@gmail.com>
2026-06-28 14:24:58 +0530

    Auto-commit: 2026-06-28 14:24:58

--- CH-9_File_IO ( Input_output )/practice set/04_problem4.py
+++ CH-9_File_IO ( Input_output )/practice set/04_problem4.py
@@ -3,7 +3,7 @@ write a program which replace this word with #### by updating
 the same file'''
 
 
-with open("file.txt") as f:
+with open("file.txt", "W") as f:
     file = f.read()
 if "Donkey" in file:
     f.write("####")
