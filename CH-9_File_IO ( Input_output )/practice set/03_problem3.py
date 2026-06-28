6e67d1e80ff387dc0e7dd95963ed02a84c87fb99
ajaybhai7 <squadterbo@gmail.com>
2026-06-28 13:17:53 +0530

    Auto-commit: 2026-06-28 13:17:53

---
+++ CH-9_File_IO ( Input_output )/practice set/03_problem3.py
@@ -0,0 +1,7 @@
+'''
+Write a program to generate multiplication table from 2 to 20
+and write it to the new different file in a folder for a 
+13 years old.. '''
+
+with open("table.txt", "w") as table:
+    table.write(table.txt)
