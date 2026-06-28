3f0704a3eb4ff59913216e31162981ab30cedacb
ajaybhai7 <squadterbo@gmail.com>
2026-06-28 14:04:56 +0530

    Auto-commit: 2026-06-28 14:04:56

--- CH-9_File_IO ( Input_output )/practice set/practice2.py
+++ CH-9_File_IO ( Input_output )/practice set/practice2.py
@@ -3,7 +3,7 @@ def tablesgenerate(n):
     for i in range(1, 11):
         table += f"{n} x {i} = {n*i}\n"
     
-    with open(f"tables/table{n}".txt, "w") as f:
+    with open(f"tables/table{n}", "w") as f:
         f.write(table)
     
 for i in range(2, 21):
