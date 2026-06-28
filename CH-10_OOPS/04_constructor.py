b4b13b4ef910e45acfa3752a7f08ea20709c6c6e
ajaybhai7 <squadterbo@gmail.com>
2026-06-28 19:48:00 +0530

    Auto-commit: 2026-06-28 19:48:00

--- CH-10_OOPS/04_constructor.py
+++ CH-10_OOPS/04_constructor.py
@@ -4,8 +4,12 @@ class employees:
     language = "python"
 
     def __init__(self, name, age, language): # Dunder method which is automatically self called
-
+        self.name = name
+        self.age = age
+        self.language = language
         print("I am creating an objecct")
+
+        
     def getinfo(self):
         print(f"This name is: {self.name}\nThis age is : {self.age}\nThis language is : {self.language}")
     @staticmethod # It's dont Need Any self Parameter in the function like
