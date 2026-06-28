395dc5f912bb8e37ffee6f92de531b16dff6ab7c
ajaybhai7 <squadterbo@gmail.com>
2026-06-28 14:22:23 +0530

    Auto-commit: 2026-06-28 14:22:23

---
+++ CH-9_File_IO ( Input_output )/practice set/04_problem4.py
@@ -0,0 +1,10 @@
+''' A file contains a word "Donkey" multiple times. You need to
+write a program which replace this word with #### by updating
+the same file'''
+
+
+with open("file.txt") as f:
+    file = f.read()
+    if "Donkey" in file:
+        with open ("file.txt", "w") as f:
+            file = f.write("####")
