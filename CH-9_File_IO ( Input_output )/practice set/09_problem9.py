4d0ca7bf4a907ad65c3fd96b0f06da8271d8d1a5
ajaybhai7 <squadterbo@gmail.com>
2026-06-28 15:43:12 +0530

    Auto-commit: 2026-06-28 15:43:12

---
+++ CH-9_File_IO ( Input_output )/practice set/09_problem9.py
@@ -0,0 +1,15 @@
+'''Write a program to find out whther a file is identical 
+and matches the content of another file'''
+
+
+with open("file.txt")as f:
+    content1 = f.read()
+
+with open("poem.txt")as f:
+    content2 = f.read()
+
+if (content1 == content2):
+    print("Yes these files are identical")
+
+else:
+    print("No these files are not identical")
