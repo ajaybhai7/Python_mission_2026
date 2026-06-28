b33d927bb62239ab2b92a992f711355b5b458f79
ajaybhai7 <squadterbo@gmail.com>
2026-06-28 19:40:53 +0530

    Auto-commit: 2026-06-28 19:40:53

---
+++ CH-10_OOPS/04_constructor.py
@@ -0,0 +1,22 @@
+class employees:
+    name = "Ajay"
+    age  = 20
+    Language = "python"
+
+    def __init__(self): # Dunder method
+        print("I am creating an objecct")
+
+    def getinfo(self):
+        print(f"This name is: {self.name}\nThis age is : {self.age}\nThis language is : {self.Language}")
+    @staticmethod # It's dont Need Any self Parameter in the function like
+    #def greet(self):
+    #   print("Good Morning")
+    def greet():
+        print("Good Morning")
+
+Ajay = employees()
+Ajay.getinfo()
+Ajay.greet()
+
+
+              
