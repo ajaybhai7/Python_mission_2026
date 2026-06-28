9e2c9c4f6841018c42106d0ac0a99e138269eb7a
ajaybhai7 <squadterbo@gmail.com>
2026-06-28 13:41:24 +0530

    Auto-commit: 2026-06-28 13:41:24

--- CH-9_File_IO ( Input_output )/practice set/03_problem3.py
+++ CH-9_File_IO ( Input_output )/practice set/03_problem3.py
@@ -2,17 +2,14 @@
 Write a program to generate multiplication table from 2 to 20
 and write it to the new different file in a folder for a 
 13 years old.. '''
-
-def multy(n, i):
-    n = 1
-    i = 1
-    while i > 10:
-        return print(f"{n} x {i} = {n*i}")
-    i = i+1
-    if i == 10:
-        return (1, 10)
-
-multy()
-    # with open("table.txt", "w") as file:
-#     file.write()
+def generatetable(n):
+    table = ""    
+    for i in range(1, n+1):
+        table += f"{n} x {i} = {n*i}\n"
     
+    with open(f"tables/tables_{n}", "w") as f:
+        f.write(table)
+
+
+    for i in range(2, 21):
+        generatefunction(n)
