290d86eccca649dbe5e3755df7323611c382d3ef
ajaybhai7 <squadterbo@gmail.com>
2026-06-29 18:29:50 +0530

    Auto-commit: 2026-06-29 18:29:50

---
+++ CH-11_inheritance_&_more_oops/inheritance.py
@@ -0,0 +1,15 @@
+class employee():
+    company = "ITC"
+    def info(self):
+        print(f"Name of employee is {self.name} and the sallary {self.sallary}")
+
+
+class programmer():
+    company = "ITC infotech"
+    def lang(self):
+        print(f"Name of employee is {self.name} and her/his language {self.language} is good")
+
+a = employee()
+b = programer()
+
+print(a.company, b.company)
