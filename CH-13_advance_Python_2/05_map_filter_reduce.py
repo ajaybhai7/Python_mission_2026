529f927d445cade3ca34ca3372fff3a7c062e357
ajaybhai7 <squadterbo@gmail.com>
2026-06-30 22:34:35 +0530

    Auto-commit: 2026-06-30 22:34:34

--- CH-13_advance_Python_2/05_map_filter_reduce.py
+++ CH-13_advance_Python_2/05_map_filter_reduce.py
@@ -8,3 +8,11 @@ square = lambda x: x*x
 sqList = map(square, l)
 print(list(sqList))
 
+# Filter Example
+
+def even(n):
+    if (n % 2 == 0):
+        return True
+    return False
+onlyeven = filter(even, l)
+print(list(onlyeven))
