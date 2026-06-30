130b153987e5f28611c57538ff77ab6ff978fa10
ajaybhai7 <squadterbo@gmail.com>
2026-06-30 20:34:59 +0530

    Auto-commit: 2026-06-30 20:34:59

--- CH-12_advance_Python/08_main.py
+++ CH-12_advance_Python/08_main.py
@@ -3,3 +3,12 @@ from module import myFunc
 # Output 
 # Hello World!
 # module
+
+# if __name__ == "__main__":
+    # myFunc()
+    # # print(__name__) # print name of file if we import anywhere 
+    # # print("We are directly running this code")
+
+# after this code the imported in it is not working 
+# because acording to the code require __name__ == "__main__"
+# But it has __name__ != "module"  
