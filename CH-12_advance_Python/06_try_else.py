116ae30db5d6bcf73efef15d317097feb661a889
ajaybhai7 <squadterbo@gmail.com>
2026-06-30 20:08:49 +0530

    Auto-commit: 2026-06-30 20:08:49

---
+++ CH-12_advance_Python/06_try_else.py
@@ -0,0 +1,14 @@
+try:
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a Second number: "))
+
+    if b == 0 or a == 0:
+        raise ZeroDivisionError("Hey our program is not " \
+        "meant to devide number by zero")
+    else:
+        print(f"The division is a/b is {a/b}")
+except ValueError:
+    print("Please Enter a Valid input!")
+
+else:
+    print("==== End of the program ====")
