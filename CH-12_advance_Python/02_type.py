313d3f282d64753bd7a6cb9d04432955c10302e8
ajaybhai7 <squadterbo@gmail.com>
2026-06-30 19:37:20 +0530

    Auto-commit: 2026-06-30 19:37:20

--- CH-12_advance_Python/02_type.py
+++ CH-12_advance_Python/02_type.py
@@ -1,3 +1,19 @@
+from typing import List, Tuple, Dict, Union
+
+# List integer
+number : list[int] = [1, 3, 5, 6]
+
+# tuple of a string and an interger
+person : tuple[str, int] = ("Alice", 34)
+
+# Dictonary with string key and integer value
+scores = dict[str, int] = {"Alice": 90, "bob": 20}
+
+# Union type for variables that can hold multiple types
+identifier: Union[str, int] = "ID986"
+identifier = 1213 # Also valid
+
+
 n : int = 5
 
 name : str = "Ajay"
