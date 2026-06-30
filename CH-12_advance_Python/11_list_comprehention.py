04ff0aaa64d2c3abc4090b4ac3a45c04e5316c04
ajaybhai7 <squadterbo@gmail.com>
2026-06-30 21:03:59 +0530

    Auto-commit: 2026-06-30 21:03:58

--- CH-12_advance_Python/11_list_comprehention.py
+++ CH-12_advance_Python/11_list_comprehention.py
@@ -9,3 +9,17 @@ print(squarelist)
 
 # Output
 
+# [1, 3, 5, 8, 3, 2]
+
+myList = [1, 3, 5, 8, 3, 2]
+
+squarelist = []
+
+for item in myList:
+    squarelist.append(item*item)
+
+print(squarelist)
+
+# Output
+
+#
