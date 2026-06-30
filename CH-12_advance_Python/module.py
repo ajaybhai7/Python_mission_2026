b49fe5863f0ad27cf9e94d2444525193b270a64b
ajaybhai7 <squadterbo@gmail.com>
2026-06-30 20:31:40 +0530

    Auto-commit: 2026-06-30 20:31:40

--- CH-12_advance_Python/module.py
+++ CH-12_advance_Python/module.py
@@ -1,9 +1,10 @@
 def myFunc():
     print("Hello World!")
 
-myFunc()
-print(__name__) # print name of file if we import anywhere 
-
+if __name__ == "__main__":
+    myFunc()
+    print(__name__) # print name of file if we import anywhere 
+    print("We are directly running this code")
 #Output: 
 #Hello World!
 #__main__
